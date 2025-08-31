f6f51fd1b751c6f37df6a62adac29f53516098f2
wapow811VR <jackmwood24@gmail.com>
2025-08-31 17:03:27 +1000

    booking page

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxw4lHuJoCrHN1KkVetUR833E8aYrRCD0",
+  authDomain: "tutoring-website-82931.firebaseapp.com",
+  projectId: "tutoring-website-82931",
+  storageBucket: "tutoring-website-82931.firebasestorage.app",
+  messagingSenderId: "772822243262",
+  appId: "1:772822243262:web:6b02ae9201d04fcde14bef"
+};
